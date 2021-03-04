09be2a1a49f5de45563872b6b55df106211e19a0
sriver7 <rsonny89@gmail.com>
2021-03-04 00:38:45 -0500

    Testing Customers Functions

--- handler.js
+++ handler.js
@@ -1,12 +1,110 @@
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-export const hello = async (event, context) => {
-  mongoose.connect(
-    process.env.ATLAS_URI,
-    {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}
-  );
-  const connection = mongoose.connection;
-  connection.once('open', () => {
-    console.log("MongoDB database connection successful!");
-  });
+'use strict';
+
+const connectToDatabase = require('./db');
+const Customer = require('./models/customer.model');
+require('dotenv').config({ path: './variables.env' });
+
+module.exports.create = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      Customer.create(JSON.parse(event.body))
+        .then(note => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(customer)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: {
+            'Content-Type': 'text/plain'
+          },
+          body: 'Could not create the customer.'
+        }));
+    });
+};
+
+module.exports.getOne = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      Customer.findById(event.pathParameters.id)
+        .then(customer => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(customer)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: {
+            'Content-Type': 'text/plain'
+          },
+          body: 'Could not fetch the customer.'
+        }));
+    });
+};
+
+module.exports.getAll = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      Customer.find()
+        .then(customers => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(customers)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: {
+            'Content-Type': 'text/plain'
+          },
+          body: 'Could not fetch the customers.'
+        }))
+    });
+};
+
+module.exports.update = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      Customer.findByIdAndUpdate(event.pathParameters.id, JSON.parse(event.body), {
+          new: true
+        })
+        .then(note => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(customer)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: {
+            'Content-Type': 'text/plain'
+          },
+          body: 'Could not fetch the customers.'
+        }));
+    });
+};
+
+module.exports.delete = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      Customer.findByIdAndRemove(event.pathParameters.id)
+        .then(note => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify({
+            message: 'Removed customer with id: ' + customer._id,
+            customer: customer
+          })
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: {
+            'Content-Type': 'text/plain'
+          },
+          body: 'Could not fetch the customer.'
+        }));
+    });
 };
